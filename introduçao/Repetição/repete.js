6772928bdd9a2cf87117a57fea981eab300b990e
Guilherme de Padua <guilhermedepadua0@gmail.com>
2021-07-13 21:07:58 -0300

    Firt commit

---
+++ introduçao/Repetição/repete.js
@@ -0,0 +1,12 @@
+var inicio = 2;
+var fim = 10;
+var todospares = "Todos pares: "
+
+while(inicio <= fim) {
+    todospares = todospares + inicio;
+
+    inicio = inicio + 2;
+
+}
+
+alert(todospares)
---
+++ introduçao/media/confirm/confirm.js
@@ -0,0 +1,6 @@
+var sair = confirm("Quer mesmo sair da pagina do google?")
+
+if(sair == 1) [
+    window.location =
+    "Http://www.google.com.br"
+]
